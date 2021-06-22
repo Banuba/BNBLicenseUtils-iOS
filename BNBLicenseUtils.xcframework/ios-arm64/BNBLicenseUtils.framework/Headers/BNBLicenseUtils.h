e7636c5ff89e02f85a33055a97f2b20ee0db7b0a
Gleb Markin <gleb.markin@banuba.com>
2021-06-22 16:24:26 +0300

    1.0.17

---
+++ BNBLicenseUtils.xcframework/ios-arm64/BNBLicenseUtils.framework/Headers/BNBLicenseUtils.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+#import <BNBLicenseUtils/BNBLicenseManager.h>
+#import <BNBLicenseUtils/BNBInformer.h>
---
+++ BNBLicenseUtils.xcframework/ios-arm64/BNBLicenseUtils.framework/Headers/all.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <bnb/license_utils/interfaces/informer.hpp>
+#include <bnb/license_utils/interfaces/license_manager.hpp>
+#include <bnb/license_utils/interfaces/types.hpp>
